cb8d9384b0674528ad37964fdd50452e6f3fd35b
adam flax <adam.flax@huddle.net>
2012-08-28 11:17:02 +0100

    added in example code for php

---
+++ folder.php
@@ -0,0 +1,88 @@
+<?php
+	class folder
+	{
+		private $parse = "";
+		
+		public function __construct($jsonblob)  
+		{
+			$this ->parse = json_decode($jsonblob, true);
+		}
+		
+		public function getLinks()
+		{
+			return count($this ->parse['links']);
+		}
+		
+		public function getLinkWithHref($href)
+		{
+			for($i = 0; $i < $this->getLinks(); $i++){
+	            if($this->parse['links'][$i]['href'] == $href){
+	                return $this->parse['links'][$i]['rel'];
+				}
+			}
+	        return NULL;
+		}
+		
+		public function getLinkWithRel($rel)
+		{
+			for($i = 0; $i < $this->getLinks(); $i++){
+	            if($this->parse['links'][$i]['rel'] == $rel){
+					return $this->parse['links'][$i]['href'];
+				}
+			}
+	        return NULL;
+		}
+		
+		public function getFolderLinks($folderId)
+		{
+			return count($this->parse['folders'][$folderId]['links']);
+		}
+		
+		public function getFolderTitle($folderId)
+		{
+			return $this->parse['folders'][$folderId]['title'];
+		}
+		
+		public function getFolderUpdated($folderId)
+		{
+			return $this->parse['folders'][$folderId]['updated'];
+		}
+		
+		public function getFolderDescription($folderId)
+		{
+			return $this->parse['folders'][$folderId]['description'];
+		}
+		
+		public function getFolderLinksHref($folderId, $linkId)
+		{
+			return $this->parse['folders'][$folderId]['links'][$linkiId]['href'];
+		}
+		
+		public function getFolderLinksRel($folderId, $linkId)
+		{
+			return $this->parse['folders'][$folderId]['links'][$linkiId]['rel'];
+		}
+		
+		public function getFolderLinkWithRel($folderId, $linkId, $rel)
+		{
+			for($i = 0; $i <  $this->getFolderLinks(); $i++){
+	            if($this->parse['folders'][$folderId]['links'][$i]['rel'] == $rel){
+					return $this->parse['folders'][$folderId]['links'][$i]['href'];
+				}
+			}
+	        return NULL;
+		}
+		
+		public function getFolderLinkWithHref($folderId, $linkId, $Href)
+		{
+			for($i = 0; $i <  $this->getFolderLinks(); $i++){
+	            if($this->parse['folders'][$folderId]['links'][$i]['href'] == $href){
+					return $this->parse['folders'][$folderId]['links'][$i]['rel'];
+				}
+			}
+	        return NULL;
+		}
+		
+		
+	}
+?>
